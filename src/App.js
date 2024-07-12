95e2b6e84bafc6672e31199a59674cccc3016ce4
Minu-Ochieng <minuochieng@gmail.com>
2024-07-12 20:26:39 +0300

    added portfolio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-
 import "./App.css";
 import Navbar from "./Navbar";
 import Podcast from "./Podcast";
-import LandingPage from './Landing-page/Index';
+import LandingPage from "./Landing-page/Index";
+import VideoSection from "./Videos";
+import Portfolio from "./Portfolio";
+
 function App() {
   return (
     <div className="App">
- <Navbar/>
- <LandingPage/>
-      <Podcast/>
-    
-    
-         </div>
+      <Navbar />
+      <LandingPage />
+      <Podcast />
+      <VideoSection />
+      <Portfolio />
+    </div>
   );
 }
 
