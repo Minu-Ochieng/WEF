6393a618e6d94002a54b7a6f027bf663014807a5
Bridget-Kathure <bridgetkathuretitus@gmail.com>
2024-07-12 08:25:36 +0300

    added changed

--- src/Video/index.js
+++ src/Video/index.js
@@ -2,33 +2,33 @@ import './index.css'
 import watch from '../Images/watch.jpg'
 import { FaPlayCircle } from "react-icons/fa";
 
+
+
+
+
 const VideoSection = () => {
-    return(
+    return (
         <div className='aboutVideo'>
-            <h1 className="heading">Explore Our Diverse Video Collection</h1>
-           
-            <div className='container'>
-
+          
+          <div className='container'>
+          <h1 className="heading">Explore Our Diverse Video Collection</h1>
             <div>
-            <img src={watch} alt="musicPic" className="videopic"/>
+              <img src={watch} alt="musicPic" className="videopic" />
             </div>
-
             <strong className='videoHeading'>Watch, Learn, and Be Captivated</strong>
             <div className='welcomeText'>
-                Crafted with unparalleled skill and vision, this video
-                stands a testament to the power of video storytelling. Delve into
-                a captavating exploration of spotify that transcends time and trends, 
-                offering a timeless perspective that will resonate with viewers for years 
-                to come.
+              Crafted with unparalleled skill and vision, this video
+              stands a testament to the power of video storytelling. Delve into
+              a captavating exploration of spotify that transcends time and trends, 
+              offering a timeless perspective that will resonate with viewers for years 
+              to come.
             </div>
-
-            <button className='watchButton'> <FaPlayCircle size={30}/>Watch</button>
-
-
-            </div>
+            <button className='watchButton'><FaPlayCircle size={30} />Watch </button>
+        
+          </div>
         </div>
-    )
-}
-
+      
+      );
+    }
 
 export default VideoSection;
